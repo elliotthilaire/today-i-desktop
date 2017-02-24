d416178913831354e807a408ff102b52d75eab71
Elliott Hilaire <elliott.hilaire@gmail.com>
2017-02-24 13:29:57 +0000

    Move config into .config folder

--- src/config_reader.js
+++ src/config_reader.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-function findStorageFile() {
+function findStorageFile (configPath) {
   if (process.env.TODAY_I_STORAGE_FILE) {
     return process.env.TODAY_I_STORAGE_FILE
   } else {
@@ -8,7 +8,7 @@ function findStorageFile() {
   }
 }
 
-function findConfigFile () {
+function findConfigFile (configPath) {
   if (process.env.TODAY_I_CONFIG) {
     return process.env.TODAY_I_CONFIG
   } else {
@@ -18,13 +18,13 @@ function findConfigFile () {
 
 module.exports = function (homePath) {
 
-  const configPath = homePath
+  const configPath = path.join(homePath, '.config', 'today-i')
 
-  const configFile = findConfigFile()
+  const configFile = findConfigFile(configPath)
   const requestConfig = require(configFile).config
 
   return {
-    storageFile: findStorageFile(),
+    storageFile: findStorageFile(configPath),
     requestConfig: requestConfig
   }
 }
