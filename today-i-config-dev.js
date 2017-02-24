073f6557f12979d803cefeaa0318c8b0e5d1f007
Elliott Hilaire <elliott.hilaire@gmail.com>
2017-02-24 15:09:36 +0000

    Change dev server route

--- today-i-config-dev.js
+++ today-i-config-dev.js
@@ -2,7 +2,7 @@ module.exports = {
 
   config: {
     method: 'POST',
-    url: 'http://localhost:3000',
+    url: 'http://localhost:3000/api/v1/',
     headers: {
       'Content-Type': 'application/json; charset=UTF-8'
     }
